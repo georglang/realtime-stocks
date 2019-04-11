42d8479cb6784cbae4868d8e33525021b1b17e53
Georg Lang <mail@georglang.com>
2019-04-11 19:20:08 +0200

    GlobalQuoteDataJsObject created

--- src/app/interfaces/SearchResult.ts
+++ src/app/interfaces/SearchResult.ts
@@ -1,4 +1,5 @@
-export class SearchResult {
+export class SearchResult
+{
   "1. symbol": "G";
   "2. name": "Genpact Limited";
   "3. type": "Equity";
@@ -8,6 +9,42 @@ export class SearchResult {
   "7. timezone": "UTC-04";
   "8. currency": "USD";
   "9. matchScore": "1.0000";
+}
+
+export class GlobalQuoteDataJsObject {
+  symbol: string;
+  open: string;
+  high: string;
+  low: string;
+  price: string;
+  volume: string;
+  latestTradingDay: string;
+  previousClose: string;
+  change: string;
+  changePercent: string;
+
+  constructor(
+    symbol: string,
+    open: string,
+    high: string,
+    low: string,
+    price: string,
+    volume: string,
+    latestTradingDay: string,
+    previousClose: string,
+    change: string,
+    changePercent: string) {
+    this.symbol = symbol;
+    this.open = open;
+    this.high = high;
+    this.low = low;
+    this.price = price;
+    this.volume = volume;
+    this.latestTradingDay = latestTradingDay;
+    this.previousClose = previousClose;
+    this.change = change;
+    this.changePercent = changePercent;
+  }
 }
 
 export interface ISearchResult {
