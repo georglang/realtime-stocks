c882844a18fba6f751ea5811efe583f558d6edf3
Georg Lang <mail@georglang.com>
2019-04-11 19:19:36 +0200

    stock-details: get and display global quoate data

--- src/app/stock-details/stock-details.component.ts
+++ src/app/stock-details/stock-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { WatchlistService } from '../services/watchlist.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { eTimeSeries } from '../interfaces/eTimeSeries';
+import { AlphavantageService } from '../services/alphavantage-service';
+import { GlobalQuoteDataJsObject } from '../interfaces/SearchResult';
+
 
 @Component({
   selector: 'app-stock-details',
@@ -6,10 +12,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-details.component.css']
 })
 export class StockDetailsComponent implements OnInit {
+  public data;
 
-  constructor() { }
+  constructor(
+    private watchlistService: WatchlistService,
+    private alphavantageService: AlphavantageService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      console.log('Params', params);
+      this.alphavantageService.getStockByWKN(params.id, eTimeSeries.INTRADAY)
+        .subscribe(data => {
+          this.mapGlobalQuoteDataToJsObject(data);
+        }, err => {
+          console.log("Error occured.", err);
+        });
+    })
   }
 
+  public mapGlobalQuoteDataToJsObject(data) {
+    const globalQuote = data["Global Quote"];
+    this.data = new GlobalQuoteDataJsObject(
+      globalQuote["01. symbol"],
+      globalQuote["02. open"],
+      globalQuote["03. high"],
+      globalQuote["04. low"],
+      globalQuote["05. price"],
+      globalQuote["06. volume"],
+      globalQuote["07. latest trading day"],
+      globalQuote["08. previous close"],
+      globalQuote["09. change"],
+      globalQuote["10. change percent"]
+    );
+
+  }
 }
