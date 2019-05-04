fd3f4a7737a451479cb8c4a7761eb797ff0d24f3
Georg Lang <mail@georglang.com>
2019-05-04 09:54:50 +0200

    trading days and hours to request realtime price

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,66 +1,135 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { eTimeSeries } from './interfaces/eTimeSeries';
 import { AlphavantageService } from './services/alphavantage-service';
 import { environment } from '../environments/environment';
+import { WatchlistService } from './services/watchlist.service';
+import { IStockInWatchlist } from './interfaces/IStockWatchlist';
+import { GlobalQuoteDataJsObject } from './interfaces/SearchResult';
+import { MessagingService } from './services/messaging-service/messaging.service';
+import { AuthService } from './core/auth-service/auth.service';
+import { ToastrService } from 'ngx-toastr';
+
+import * as moment from 'moment-business-days';
 
 // impoart { c } from 'highcharts';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.sass']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit {
-    public searchResult = [];
-    private API_KEY = environment.alphavantageApiKey;
+  public searchResult = [];
+  private API_KEY = environment.alphavantageApiKey;
+  public message;
+  public isLoggedIn = false;
+  private startTime;
+  private endTime;
+  private realtimeStock: GlobalQuoteDataJsObject;
 
+  constructor(
+    private alphavantageService: AlphavantageService,
+    private watchlistService: WatchlistService,
+    private messagingService: MessagingService,
+    public authService: AuthService,
+    private toastr: ToastrService
+  ) {
+    this.configMomentJs();
+  }
 
-    constructor(private http: HttpClient, private alphavantageService: AlphavantageService) { }
-
-    ngOnInit(): void {
-        //     Highcharts.chart('container', {
-        //       chart: {
-        //           type: 'bar'
-        //       },
-        //       title: {
-        //           text: 'Fruit Consumption'
-        //       },
-        //       xAxis: {
-        //           categories: ['Apples', 'Bananas', 'Oranges']
-        //       },
-        //       yAxis: {
-        //           title: {
-        //               text: 'Fruit eaten'
-        //           }
-        //       },
-        //       series: [{
-        //           name: 'Jane',
-        //           data: [1, 0, 4]
-        //       }, {
-        //           name: 'John',
-        //           data: [5, 7, 3]
-        //       }]
-        //   });
+  ngOnInit() {
+    const userId = localStorage.getItem('id');
+    if (userId !== undefined) {
+      this.messagingService.requestPermission(userId)
+      this.messagingService.receiveMessage()
+      this.message = this.messagingService.currentMessage;
     }
 
-    // get list of all stocks and save to indexedDB
+    console.log('Time', moment(moment(), 'DD-MM-YYYY').isBusinessDay());
+    this.getCurrentPrices();
+  }
+
+  public configMomentJs() {
+    moment.updateLocale('de', {
+      workingWeekdays: [1, 2, 3, 4, 5]
+    });
+  }
+
+  public login() {
+    this.authService.signInWithGoogle();
+  }
+
+  private requestStockPrices() {
+    let stockInWatchlist: IStockInWatchlist;
+    if (this.watchlistService !== undefined) {
+      this.watchlistService.getWatchlist()
+        .then(stocks => {
+          stocks.forEach(stock => {
+            stockInWatchlist = Object.assign(stock);
 
 
 
-    public setUrl() {
+
+            this.alphavantageService.getStockBySymbol(stock.symbol)
+              .subscribe((realtimeStock) => {
+                if (realtimeStock !== undefined) {
+                  this.realtimeStock = this.mapGlobalQuoteDataToJsObject(realtimeStock);
+
+                  if (stockInWatchlist.useLowLimit) {
+                    if (this.realtimeStock.price <= stockInWatchlist.limitLow) {
+                      this.toastr.warning('Unteren Schwellwert unterschritten', 'Aktie: ' + stockInWatchlist.name);
+                    }
+                  } else {
+                    if (this.realtimeStock.price >= stockInWatchlist.limitHigh) {
+                      this.toastr.warning('Oberen Schwellwert überschritten', 'Aktie');
+                    }
+                  }
+                }
+              });
+          });
+        });
     }
+  }
 
-    public interday() {
-        this.alphavantageService.callApi('AAPL', eTimeSeries.INTRADAY);
+  // trading days mo - fri
+  // trading times 07:30 - 23:00
+  private getCurrentPrices() {
+    // Trading days mo - fri
+    if (moment(moment(), 'DD-MM-YYYY').isBusinessDay()) {
+
+      // Saturday and Sunday
+    } else {
+      console.log('Not a business day');
+      const startTime = moment('07:30:00', 'HH:mm:ss');
+      const endTime = moment('23:00:00', 'HH:mm:ss');
+
+
+      if (moment().isBetween(startTime, endTime)) {
+        console.log(' ******* Is Between ******');
+        this.requestStockPrices();
+
+      } else {
+        console.log('###### OUt of Business Hours #####');
+
+      }
     }
+    setInterval(this.getCurrentPrices, 10000);
+  }
 
-    public weekley() {
-        this.alphavantageService.callApi('AAPL', eTimeSeries.WEEKLY);
+  public mapGlobalQuoteDataToJsObject(data) {
+    if (data !== undefined) {
+      const globalQuote = data["Global Quote"];
+      return new GlobalQuoteDataJsObject(
+        globalQuote["01. symbol"],
+        globalQuote["02. open"],
+        globalQuote["03. high"],
+        globalQuote["04. low"],
+        globalQuote["05. price"],
+        globalQuote["06. volume"],
+        globalQuote["07. latest trading day"],
+        globalQuote["08. previous close"],
+        globalQuote["09. change"],
+        globalQuote["10. change percent"]
+      );
     }
-
-
-
-
-
+  }
 }
