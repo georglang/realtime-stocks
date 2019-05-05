40bc3dce4c58f4a086ead29a63c66eced77e1175
Georg Lang <mail@georglang.com>
2019-05-05 18:39:56 +0200

    IStockWatchlist extended with currency

--- src/app/interfaces/IStockWatchlist.ts
+++ src/app/interfaces/IStockWatchlist.ts
@@ -1,6 +1,7 @@
 export interface IStockInWatchlist {
   symbol: string,
   name: string,
+  currency: string,
   limitHigh: string,
   limitLow: string,
   useLowLimit: boolean
@@ -9,12 +10,14 @@ export interface IStockInWatchlist {
 export class StockInWatchlist implements IStockInWatchlist {
   public symbol: string;
   public name: string;
+  currency: string;
   public limitHigh: string;
   public limitLow: string;
   public useLowLimit: boolean;
-  constructor(symbol: string, name: string, limitHigh: string, limitLow: string, useLowLimit: boolean) {
+  constructor(symbol: string, name: string, currency: string, limitHigh: string, limitLow: string, useLowLimit: boolean) {
     this.symbol = symbol,
     this.name = name,
+    this.currency = currency,
     this.limitHigh = limitHigh,
     this.limitLow = limitLow,
     this.useLowLimit = useLowLimit
