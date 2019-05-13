289309fbf1c026d293d09460ab00f3319117b35c
Georg Lang <mail@georglang.com>
2019-05-13 20:10:36 +0200

    app.routing updated

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { StockListComponent } from './stock-list/stock-list.component';
 import { WatchlistComponent } from './watchlist/watchlist.component';
 import { StockDetailsComponent } from './stock-details/stock-details.component';
+import { SearchComponent } from './search/search.component';
 
 const appRoutes = [
-  { path: '', component: StockListComponent },
+
+  {path: '', redirectTo: 'watchlist', pathMatch: 'full'},
   {
     path: 'stock-details/:id', component: StockDetailsComponent
   },
-  { path: 'watchlist', component: WatchlistComponent },
-  // * wildcard if the requested URL doesn´t match any path in the URL
-  // could also be a 404 page
-  { path: '**', component: StockListComponent, pathMatch: 'full' }
+  {
+    path: 'search', component: SearchComponent
+  },
+  {
+    path: 'watchlist', component: WatchlistComponent
+  }
+  // { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
