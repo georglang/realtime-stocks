2d9dcc2376bc699b94a9ba1aae815c80cdaec7cf
Georg Lang <mail@georglang.com>
2019-05-16 21:24:02 +0200

    ILimitReachedStock added

---
+++ src/app/interfaces/ILimitReachedStock.ts
@@ -0,0 +1,12 @@
+export interface ILimitReachedStock {
+  change: string;
+  changePercent: string;
+  high: string;
+  latestTradingDay: string;
+  low: string;
+  open: string;
+  previousClose: string;
+  price: string;
+  symbol: string;
+  volume: string;
+}
